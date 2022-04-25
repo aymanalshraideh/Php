a9b5a141434a28777c7b7ffa7617941ed0828938
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-04-25 18:12:55 +0300

    main

---
+++ My Code/Object.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <title>Document</title>
+</head>
+<body>
+   <div><?php 
+     $cars=array("BMW","volvo");
+     
+    
+    ?></div> 
+</body>
+</html>
---
+++ My Code/first.php
@@ -0,0 +1,8 @@
+<html>
+
+<h1>
+<?php 
+echo 'Ayman'
+?>
+</h1>
+</html>
